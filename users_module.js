8b1c0f09f8d9f6093fd1fe69835cdc1e8fe0b5af
PriyalTodi <88275929+PriyalTodi@users.noreply.github.com>
2022-04-19 23:17:16 +0530

    added comments

--- users_module.js
+++ users_module.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 
 //mongodb://127.0.0.1:27017/dbname
 //const conn_str = "mongodb://localhost:27017/tcet";
+//add your mongodb str below
+
 const conn_str = "mongodb://tcet_user:Test_user@mycluster-shard-00-00.avnxf.mongodb.net:27017,mycluster-shard-00-01.avnxf.mongodb.net:27017,mycluster-shard-00-02.avnxf.mongodb.net:27017/tcet_system?ssl=true&replicaSet=atlas-uipcg5-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-
+//connection verification
 mongoose.connect(conn_str, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log("Connected successfully..."))
 	.catch( (error) => console.log(error) );
